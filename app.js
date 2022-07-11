508b24e8ae148ce43984b8a6723fcf285c1ad5e7
efmoteam <myozinthant1812001@gmail.com>
2022-07-11 20:54:11 +0630

    Modal Box Style and Function

---
+++ app.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector(".modal");
+const openBtn = document.querySelector(".open-btn");
+const closeBtn = document.querySelector(".close-btn");
+
+openBtn.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.close();
+});
